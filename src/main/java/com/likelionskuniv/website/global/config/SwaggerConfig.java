7b3eccfddf51a749fde566ec5631486eb19b3f2c
Ksieon <keumsiun0503@gmail.com>
2026-01-07 18:51:03 +0900

    :wrench: Settings: Swagger, Security, Cors 초기 설정

--- src/main/java/com/likelionskuniv/website/global/config/SwaggerConfig.java
+++ src/main/java/com/likelionskuniv/website/global/config/SwaggerConfig.java
@@ -1,5 +1,51 @@
+/* 
+ * Copyright (c) SKU LIKELION 
+ */
 package com.likelionskuniv.website.global.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
 public class SwaggerConfig {
 
+  @Value("${server.servlet.context-path:}")
+  private String contextPath;
+
+  @Value("${swagger.server.profile}")
+  private String profileUrl;
+
+  @Value("${swagger.server.name}")
+  private String profileName;
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    Server server = new Server().url(profileUrl + contextPath).description(profileName + " Server");
+
+    return new OpenAPI()
+        .addServersItem(server)
+        .info(
+            new Info()
+                .title("Likelion Skuniv Website API 명세서")
+                .version("1.0")
+                .description(
+                    """
+                        # 멋쟁이사자처럼 서경대학교 웹페이지 프로젝트 API 문서
+
+                        ## 주의사항
+                        - 파일 업로드 크기 제한: 5MB (1개 파일 크기)
+
+                        """));
+  }
+
+  @Bean
+  public GroupedOpenApi apiGroup() {
+    return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
+  }
 }
