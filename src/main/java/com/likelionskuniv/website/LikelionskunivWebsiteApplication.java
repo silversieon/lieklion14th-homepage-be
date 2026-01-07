fcc4d23a1c0981e8ec37420b1cebe2d8d1208487
Ksieon <keumsiun0503@gmail.com>
2026-01-07 17:13:51 +0900

    :tada: Start: 프로젝트 시작 설정

---
+++ src/main/java/com/likelionskuniv/website/LikelionskunivWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.likelionskuniv.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionskunivWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionskunivWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelionskuniv/website/LikelionskunivWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelionskuniv.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionskunivWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
