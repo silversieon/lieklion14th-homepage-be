aceb9a92e7efe97de569ddbf9074d78a988441ec
Sieon Keum <keumsiun0503@gmail.com>
2026-02-03 22:51:25 +0900

    ♻️ Refactor: CORS 정책 변경
    * :gear: Settings: SecurityFilter에 actuator/health permitAll 추가
    
    * :wrench: Settings: JwtProperties 추가
    
    * :recycle: Refactor: 추가한 환경변수를 적용한 쿠키 응답 방식 변경
    
    * :sparkles: Feat: 임의의 이메일 인증 처리 메서드 추가
    
    * :sparkles: Feat: 임의의 메일 인증 처리 API 추가
    
    * :wrench: Settings: cookie secure, same-site 설정 추가
    
    * :recycle: Refactor: setAllowCredentials true를 통한 자격증명 요청 허용, OPTIONS 허용을 통한 프리플라이트 허용, 헤더 전체 허용

--- src/main/java/com/skunivlikelion/homepage/global/config/CorsConfig.java
+++ src/main/java/com/skunivlikelion/homepage/global/config/CorsConfig.java
@@ -4,6 +4,7 @@
 package com.skunivlikelion.homepage.global.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,10 +27,12 @@ public class CorsConfig {
 
     // 환경 변수에 정의된 출처만 허용
     configuration.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins()));
+    configuration.setAllowCredentials(true);
     // 리스트에 작성한 HTTP 메소드 요청만 허용
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+    configuration.setAllowedMethods(
+        Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     // 리스트에 작성한 헤더들이 포함된 요청만 허용
-    configuration.setAllowedHeaders(Arrays.asList("Content-Type", "X-Requested-With"));
+    configuration.setAllowedHeaders(List.of("*"));
     // 모든 경로에 대해 위의 CORS 설정을 적용
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
