db69c18ff16c6e7e72a804932ff8f496ffe3e5d0
Sieon Keum <keumsiun0503@gmail.com>
2026-02-17 12:42:24 +0900

    ♻️ Refactor: 마이페이지 응답 조회 로직 변경
    * :recycle: Refactor: 면접 일정 확정 여부 필드 추가
    
    * :recycle:Refactor: 면접 일정 확정 여부 빌더패턴에 추가
    
    * :recycle: Refactor: 면접 일정 확정 여부 로직 추가
    
    * :recycle: Refactor: 면접 일정 확정 여부 명세 추가

--- src/main/java/com/skunivlikelion/homepage/domain/user/dto/response/MyPageResponse.java
+++ src/main/java/com/skunivlikelion/homepage/domain/user/dto/response/MyPageResponse.java
@@ -30,6 +30,9 @@ public class MyPageResponse {
   @Schema(description = "면접 일정 변경 가능 여부", example = "false")
   private boolean interviewScheduleChangeable;
 
+  @Schema(description = "면접 일정 확정 여부", example = "false")
+  private boolean interviewScheduleConfirmed;
+
   @Schema(description = "최종 결과 확인 가능 여부", example = "true")
   private boolean finalResultConfirmation;
 }
