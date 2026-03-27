b8f470be7482e79853a86fcf60f37c000bbca319
Comeoksal <keumsiun0503@gmail.com>
2026-03-27 16:41:30 +0900

    :recycle: Refactor: rename & delete

--- src/main/java/com/skunivlikelion/homepage/domain/project/dto/internal/UploadImagePayload.java
+++ src/main/java/com/skunivlikelion/homepage/domain/project/dto/internal/UploadImagePayload.java
@@ -1,6 +1,6 @@
 /* 
  * Copyright (c) SKU LIKELION 
  */
-package com.skunivlikelion.homepage.domain.project.dto.request;
+package com.skunivlikelion.homepage.domain.project.vo;
 
 public record UploadImagePayload(String originalFilename, String contentType, byte[] bytes) {}
